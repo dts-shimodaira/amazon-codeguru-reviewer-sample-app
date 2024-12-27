80f7dcd7ec02f8df2131c75facbc8c42b5e1272f
dts-shimodaira <ke-shimodaira@sec.dts.co.jp>
2024-12-27 14:09:57 +0900

    Update test.py

--- src/test/resources/test.py
+++ src/test/resources/test.py
@@ -1,6 +1,6 @@
 import boto3
 
-session = boto3.session.Session(aws_access_key_id='XXXXXXXXXXXXXXXXXXXX', aws_secret_access_key='YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY')
+session = boto3.session.Session(aws_access_key_id='AKIASU566IV2ZQPCWQfd', aws_secret_access_key='ZO+zFDVZxe8qJ9W9wWxwaYaNc9lxB1zOxTz/9gfr')
 client = session.client('iam')
 resp = client.list_users()
 print("Able to access iam user list:")
